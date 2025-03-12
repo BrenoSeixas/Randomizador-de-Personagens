8330e8df5e8014b050173be31fcbf5e37fafd7ab
Breno Seixas <olegarioseixasb@gmail.com>
2025-03-12 13:13:54 -0300

    Commit Inicial

---
+++ Randomizador/Randomizador/Program.cs
@@ -0,0 +1,24 @@
+﻿using Randomizador;
+using System.ComponentModel;
+using System.Threading.Channels;
+
+namespace Randomizer
+{
+    class Program {
+    
+        static void Main(string[] args)
+        {
+            TesteVector teste = new TesteVector();
+
+
+            Console.WriteLine("Digite 0 para randomizar um número:");
+
+
+            teste.imagem[2] = 3;
+
+            Console.WriteLine(teste.imagem[2].ToString());
+        }
+    
+    }
+
+}
---
+++ Randomizador/Randomizador/TesteVector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizador
+{
+    internal class TesteVector
+    {
+        public int[] imagem = new int[10];
+
+
+        public TesteVector()
+        {
+
+        }
+
+        public TesteVector(int[] imagem)
+        {
+            this.imagem = imagem;
+        }
+    }
+}
